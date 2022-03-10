c72a6b43424f47735b76ce64c79e35613bb4b6da
Diego Espitia <despitia@uniminuto.edu>
2022-03-10 13:44:59 -0500

    Chat sin media

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
-var btnPpal = document.getElementById('btn-ppal');
-var btnWhat = document.getElementById('btn-wsp');
-var btnChat = document.getElementById('btn-chat');
+var btnPpal = document.getElementById('btn-ppal-chatbot');
+var btnWhat = document.getElementById('btn-wsp-chatbot');
+var btnChat = document.getElementById('btn-chat-chatbot');
 btnPpal.addEventListener('change', cambioEstadoCheckPpal);
 btnWhat.addEventListener('change', cambioEstadoCheckWhat);
 btnChat.addEventListener('change', cambioEstadoCheckChat);
@@ -9,17 +9,17 @@ btnChat.addEventListener('change', cambioEstadoCheckChat);
 function cambioEstadoCheckPpal() {
     if (this.checked) {
         console.log("Se presionó el check");
-        document.getElementById("btn-wsp").checked = false;
-        document.getElementById("btn-chat").checked = false;
+        document.getElementById("btn-wsp-chatbot").checked = false;
+        document.getElementById("btn-chat-chatbot").checked = false;
     }
 }
 function cambioEstadoCheckWhat() {
     if (this.checked) {
-        document.getElementById("btn-chat").checked = false;
+        document.getElementById("btn-chat-chatbot").checked = false;
     }
 }
 function cambioEstadoCheckChat() {
     if (this.checked) {
-        document.getElementById("btn-wsp").checked = false;
+        document.getElementById("btn-wsp-chatbot").checked = false;
     }
 }
